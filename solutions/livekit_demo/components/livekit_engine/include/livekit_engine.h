7c65263cd888582a6bbbb8893a131d6c55bbb94f
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-05-31 13:50:04 +1000

    Refactor engine

--- solutions/livekit_demo/components/livekit_engine/include/livekit_engine.h
+++ solutions/livekit_demo/components/livekit_engine/include/livekit_engine.h
@@ -1,26 +1,3 @@
-/**
- * ESPRESSIF MIT License
- *
- * Copyright (c) 2025 <ESPRESSIF SYSTEMS (SHANGHAI) CO., LTD>
- *
- * Permission is hereby granted for use on all ESPRESSIF SYSTEMS products, in which case,
- * it is free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
- * and/or sell copies of the Software, and to permit persons to whom the Software is furnished
- * to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
- * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
- * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
- * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
- * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- */
 
 #pragma once
 
@@ -29,239 +6,132 @@
 #include "esp_capture.h"
 #include "av_render.h"
 
+#include "livekit_protocol.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/**
- * @brief  ESP WebRTC handle
- */
+/// @brief  Handle to an engine instance.
 typedef void *livekit_eng_handle_t;
 
 typedef enum {
-    LIVEKIT_ENG_CUSTOM_DATA_VIA_NONE,
-    LIVEKIT_ENG_CUSTOM_DATA_VIA_SIGNALING,
-    LIVEKIT_ENG_CUSTOM_DATA_VIA_DATA_CHANNEL,
-} livekit_eng_custom_data_via_t;
+    LIVEKIT_ENG_ERR_NONE        =  0,
+    LIVEKIT_ENG_ERR_INVALID_ARG = -1,
+    LIVEKIT_ENG_ERR_NO_MEM      = -2,
+    LIVEKIT_ENG_ERR_SIGNALING   = -3,
+    LIVEKIT_ENG_ERR_OTHER       = -4,
+    // TODO: Add more error cases as needed
+} livekit_eng_err_t;
 
-/**
- * @brief  ESP WebRTC peer connection configuration
- */
-typedef struct {
-    esp_peer_ice_server_cfg_t   *server_lists;            /*!< STUN/Relay server URL lists, can be NULL when get from signaling */
-    uint8_t                      server_num;              /*!< Number of STUN/Relay server URL */
-    esp_peer_ice_trans_policy_t  ice_trans_policy;        /*!< ICE transport policy */
-    esp_peer_audio_stream_info_t audio_info;              /*!< Audio stream information for send */
-    esp_peer_video_stream_info_t video_info;              /*!< Video stream information for send */
-    esp_peer_media_dir_t         audio_dir;               /*!< Audio transmission direction */
-    esp_peer_media_dir_t         video_dir;               /*!< Video transmission direction */
-    bool                         enable_data_channel;     /*!< Whether enable data channel */
-    bool                         manual_ch_create;        /*!< When set, disable auto create data channel in SCTP client mode if `enable_data_channel` set
-                                                               User need manually call `esp_peer_create_data_channel` instead */
-    bool                         video_over_data_channel; /*!< Whether send and receive video data through data channel */
-    bool                         no_auto_reconnect;       /*!< Disable auto reconnect
-                                                               In room related WebRTC application, connection build up with peer
-                                                               If peer leaves, it will auto re-enter same room (send new SDP) after clear up
-                                                               Disable reconnect will do nothing after clear up until call `livekit_eng_enable_peer_connection` */
-    void                        *extra_cfg;               /*!< Extra configuration for peer connection */
-    int                          extra_size;              /*!< Size of extra configuration */
-    void                        *ctx;                     /*!< User context */
-
-    /**
-     * @brief  This API is used for users who do not care the data channel or signaling details
-     *         And want to receive data from them only
-     */
-    int (*on_custom_data)(livekit_eng_custom_data_via_t via, uint8_t *data, int size, void *ctx);
-
-    /**
-     * @brief  Following API are function groups for users who want more control over data channels
-     */
-    int (*on_channel_open)(esp_peer_data_channel_info_t *ch, void *ctx);   /*!< Callback invoked when a data channel is opened */
-    int (*on_data)(esp_peer_data_frame_t *frame, void *ctx);               /*!< Callback invoked when data is received on the channel */
-    int (*on_channel_close)(esp_peer_data_channel_info_t *ch, void *ctx);  /*!< Callback invoked when a data channel is closed */
-} livekit_eng_peer_cfg_t;
-
-/**
- * @brief  ESP WebRTC signaling configuration
- */
-typedef struct {
-    char *signal_url; /*!< Signaling server URL */
-    void *extra_cfg;  /*!< Extra configuration for special signaling server */
-    int   extra_size; /*!< Size of extra configuration */
-    void *ctx;        /*!< User context */
-} livekit_eng_signaling_cfg_t;
-
-/**
- * @brief  ESP WebRTC configuration
- */
-typedef struct {
-    livekit_eng_signaling_cfg_t       signaling_cfg;  /*!< Signaling configuration */
-    livekit_eng_peer_cfg_t            peer_cfg;       /*!< Peer connection configuration */
-} livekit_eng_cfg_t;
-
-/**
- * @brief  WebRTC event type
- */
-typedef enum {
-    LIVEKIT_ENG_EVENT_NONE                      = 0, /*!< None event */
-    LIVEKIT_ENG_EVENT_CONNECTED                 = 1, /*!< Connected event */
-    LIVEKIT_ENG_EVENT_CONNECT_FAILED            = 2, /*!< Connected failed event */
-    LIVEKIT_ENG_EVENT_DISCONNECTED              = 3, /*!< Disconnected event */
-    LIVEKIT_ENG_EVENT_DATA_CHANNEL_CONNECTED    = 4, /*!< Data channel connected event */
-    LIVEKIT_ENG_EVENT_DATA_CHANNEL_DISCONNECTED = 5, /*!< Data channel disconnected event */
-    LIVEKIT_ENG_EVENT_DATA_CHANNEL_OPENED       = 6, /*!< Data channel opened event, suitable for one data channel only */
-    LIVEKIT_ENG_EVENT_DATA_CHANNEL_CLOSED       = 7, /*!< Data channel closed event, suitable for one data channel only */
-} livekit_eng_event_type_t;
-
-/**
- * @brief  WebRTC event
- */
-typedef struct {
-    livekit_eng_event_type_t type; /*!< Event type */
-    char                   *body; /*!< Event body (maybe NULL) */
-} livekit_eng_event_t;
-
-/**
- * @brief  WebRTC media provider
- *
- * @note  Media player and capture system are created from outside.
- *        WebRTC will internally use the capture and player handle to capture media data and do media playback
- */
+/// @brief  WebRTC media provider
+/// @note   Media player and capture system are created externally.
+///         WebRTC will internally use the capture and player handle to capture media data and perform media playback.
 typedef struct {
     esp_capture_handle_t capture; /*!< Capture system handle */
     av_render_handle_t   player;  /*!< Player handle */
 } livekit_eng_media_provider_t;
 
-/**
- * @brief  WebRTC event handler
- *
- * @param[in]  event  WebRTC event
- * @param[in]  ctx    User context
- *
- * @return  Status to indicate success or failure
- */
-typedef int (*livekit_eng_event_handler_t)(livekit_eng_event_t *event, void *ctx);
+typedef struct {
+    // This is an alternative to RtcEngine's async connect method
+    livekit_join_response_t join_response;
+} livekit_eng_event_connected_t;
 
-/**
- * @brief  WebRTC event handler
- *
- * @param[in]   event       WebRTC event
- * @param[out]  rtc_handle  WebRTC handle
- *
- * @return
- *      - ESP_PEER_ERR_NONE         On success
- *      - ESP_PEER_ERR_INVALID_ARG  Invalid argument
- *      - ESP_PEER_ERR_NO_MEM       Not enough memory
- */
-int livekit_eng_open(livekit_eng_cfg_t *cfg, livekit_eng_handle_t *rtc_handle);
+typedef struct {
+    livekit_disconnect_reason_t reason;
+} livekit_eng_event_disconnected_t;
 
-/**
- * @brief  WebRTC set media provider
- *
- * @param[in]  rtc_handle  WebRTC handle
- * @param[in]  provider    Media player and capture provider setting
- *
- * @return
- *      - ESP_PEER_ERR_NONE         On success
- *      - ESP_PEER_ERR_INVALID_ARG  Invalid argument
- */
-int livekit_eng_set_media_provider(livekit_eng_handle_t rtc_handle, livekit_eng_media_provider_t *provider);
+typedef struct {
+    // This is an alternative to RtcEngine's async connect method
+    // returning a error result.
+    // TODO: add error details
+} livekit_eng_event_error_t;
 
-/**
- * @brief  WebRTC set event handler
- *
- * @param[in]  rtc_handle  WebRTC handle
- * @param[in]  handler     Event handler
- * @param[in]  ctx         Event user context
- *
- * @return
- *      - ESP_PEER_ERR_NONE         On success
- *      - ESP_PEER_ERR_INVALID_ARG  Invalid argument
- */
-int livekit_eng_set_event_handler(livekit_eng_handle_t rtc_handle, livekit_eng_event_handler_t handler, void *ctx);
+typedef struct {
+    livekit_room_t *updated_room;
+} livekit_eng_event_room_update_t;
 
-/**
- * @brief  Start WebRTC
- *
- * @param[in]  rtc_handle  WebRTC handle
- *
- * @return
- *      - ESP_PEER_ERR_NONE         On success
- *      - ESP_PEER_ERR_INVALID_ARG  Invalid argument
- *      - ESP_PEER_ERR_NO_MEM       Not enough memory
- */
-int livekit_eng_enable_peer_connection(livekit_eng_handle_t rtc_handle, bool enable);
+typedef struct {
+    char* participant_sid;
+    uint8_t *data;
+    int size;
+} livekit_eng_event_data_t;
 
-/**
- * @brief  Start WebRTC
- *
- * @param[in]  rtc_handle  WebRTC handle
- *
- * @return
- *      - ESP_PEER_ERR_NONE         On success
- *      - ESP_PEER_ERR_INVALID_ARG  Invalid argument
- *      - Others                    Fail to start
- */
-int livekit_eng_start(livekit_eng_handle_t rtc_handle);
+typedef struct {
+    char* caller_sid;
+    char* request_id;
+    char* method;
+    char* payload;
+    uint32_t response_timeout;
+    uint32_t version;
+} livekit_eng_event_rpc_request_t;
 
-/**
- * @brief  Send customized data
- *
- * @param[in]  rtc_handle  WebRTC handle
- *
- * @return
- *      - ESP_PEER_ERR_NONE         On success
- *      - ESP_PEER_ERR_INVALID_ARG  Invalid argument
- *      - Others                    Fail to send customized data
- */
-int livekit_eng_send_custom_data(livekit_eng_handle_t rtc_handle, livekit_eng_custom_data_via_t via, uint8_t *data, int size);
+typedef struct {
+    char* request_id;
+    char* payload;
+    livekit_rpc_error_t* error;
+} livekit_eng_event_rpc_response_t;
 
-/**
- * @brief  Get Peer Connection handle
- *
- * @param[in]  rtc_handle   WebRTC handle
- * @param[in]  peer_handle  Peer connection handle
- *
- * @return
- *      - ESP_PEER_ERR_NONE         On success
- *      - ESP_PEER_ERR_INVALID_ARG  Invalid argument
- *      - ESP_PEER_ERR_WRONG_STATE  Wrong state for peer connection not build yet
- */
-int livekit_eng_get_peer_connection(livekit_eng_handle_t rtc_handle, esp_peer_handle_t *peer_handle);
+typedef struct {
+    char* request_id;
+} livekit_eng_event_rpc_ack_t;
 
-/**
- * @brief  Query status of WebRTC
- *
- * @param[in]  rtc_handle  WebRTC handle
- *
- * @return
- *      - ESP_PEER_ERR_NONE         On success
- *      - ESP_PEER_ERR_INVALID_ARG  Invalid argument
- */
-int livekit_eng_query(livekit_eng_handle_t rtc_handle);
+typedef struct {
+    livekit_data_stream_header_t header;
+    char* participant_identity;
+} livekit_eng_event_stream_header_t;
 
-/**
- * @brief  Stop WebRTC
- *
- * @param[in]  rtc_handle  WebRTC handle
- *
- * @return
- *      - ESP_PEER_ERR_NONE         On success
- *      - ESP_PEER_ERR_INVALID_ARG  Invalid argument
- *      - Others                    Fail to send customized data
- */
-int livekit_eng_stop(livekit_eng_handle_t rtc_handle);
+typedef struct {
+    livekit_data_stream_chunk_t chunk;
+    char* participant_identity;
+} livekit_eng_event_stream_chunk_t;
 
-/**
- * @brief  Close WebRTC
- *
- * @param[in]  rtc_handle  WebRTC handle
- *
- * @return
- *      - ESP_PEER_ERR_NONE         On success
- *      - ESP_PEER_ERR_INVALID_ARG  Invalid argument
- */
-int livekit_eng_close(livekit_eng_handle_t rtc_handle);
+typedef struct {
+    livekit_data_stream_trailer_t trailer;
+    char* participant_identity;
+} livekit_eng_event_stream_trailer_t;
+
+typedef struct {
+    void *ctx;
+
+    void (*on_connected)(livekit_eng_event_connected_t detail, void *ctx);
+    void (*on_disconnected)(livekit_eng_event_disconnected_t detail, void *ctx);
+    void (*on_error)(livekit_eng_event_error_t detail, void *ctx);
+
+    void (*on_room_update)(livekit_eng_event_room_update_t detail, void *ctx);
+    void (*on_data)(livekit_eng_event_data_t detail, void *ctx);
+    void (*on_rpc_request)(livekit_eng_event_rpc_request_t detail, void *ctx);
+    void (*on_rpc_response)(livekit_eng_event_rpc_response_t detail, void *ctx);
+    void (*on_rpc_ack)(livekit_eng_event_rpc_ack_t detail, void *ctx);
+    void (*on_stream_header)(livekit_eng_event_stream_header_t detail, void *ctx);
+    void (*on_stream_chunk)(livekit_eng_event_stream_chunk_t detail, void *ctx);
+    void (*on_stream_trailer)(livekit_eng_event_stream_trailer_t detail, void *ctx);
+
+} livekit_eng_options_t;
+
+/// @brief Creates a new instance.
+/// @param[out] handle The handle to the new instance.
+int livekit_eng_create(livekit_eng_options_t *options, livekit_eng_handle_t *handle);
+
+/// @brief Destroys an instance.
+/// @param[in] handle The handle to the instance to destroy.
+int livekit_eng_destroy(livekit_eng_handle_t handle);
+
+/// @brief Connect the engine.
+int livekit_eng_connect(const char* server_url, const char* token, livekit_eng_handle_t handle);
+
+/// @brief Close the engine.
+/// @param reason Reason for why the engine is being closed.
+int livekit_eng_close(livekit_disconnect_reason_t reason, livekit_eng_handle_t handle);
+
+/// @brief Publishes a data packet over the data channel.
+int livekit_eng_publish_data(livekit_data_packet_t packet, livekit_data_packet_kind_t kind, livekit_eng_handle_t handle);
+
+/// @brief Sends a signaling request.
+int livekit_eng_send_request(livekit_signal_request_t request, livekit_eng_handle_t handle);
+
+/// @brief Sets the media provider.
+int livekit_eng_set_media_provider(livekit_eng_media_provider_t* provider, livekit_eng_handle_t handle);
 
 #ifdef __cplusplus
 }
