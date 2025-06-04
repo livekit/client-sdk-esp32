b0d199b5983ead2fba4b4fcd8098e19249ad3ed7
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-06-04 16:33:20 +1000

    Print string description of incoming signal res

--- solutions/livekit_demo/components/livekit_protocol/include/livekit_protocol.h
+++ solutions/livekit_demo/components/livekit_protocol/include/livekit_protocol.h
@@ -8,3 +8,6 @@
 #include "livekit_models.pb.h"
 #include "livekit_metrics.pb.h"
 #include "timestamp.pb.h"
+
+/// @brief Gets the name of the signaling response type.
+const char* livekit_protocol_sig_res_name(pb_size_t which_message);
