b92378056e57463a95c9c7ccc275c4796601fcfc
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-07-25 12:49:20 -0700

    LVGL task affinity

--- examples/voice_agent_lcd/main/board.c
+++ examples/voice_agent_lcd/main/board.c
@@ -16,19 +16,27 @@ void board_init()
 {
     ESP_LOGI(TAG, "Initializing board");
 
-    // Initialize board support package, LCD, and UI.
     bsp_i2c_init();
-    bsp_display_start();
+
+    bsp_display_cfg_t display_cfg = {
+        .lvgl_port_cfg = ESP_LVGL_PORT_INIT_CONFIG(),
+        .buffer_size = BSP_LCD_H_RES * CONFIG_BSP_LCD_DRAW_BUF_HEIGHT,
+        .double_buffer = 0,
+        .flags = { .buff_dma = true }
+    };
+    display_cfg.lvgl_port_cfg.task_affinity = 1; // Put LVGL on core 1
+    bsp_display_start_with_config(&display_cfg);
+
     bsp_display_backlight_on();
 
     // Initialize codec board
     set_codec_board_type(CONFIG_CODEC_BOARD_TYPE);
-    codec_init_cfg_t cfg = {
+    codec_init_cfg_t codec_cfg = {
         .in_mode = CODEC_I2S_MODE_TDM,
         .in_use_tdm = true,
         .reuse_dev = false
     };
-    init_codec(&cfg);
+    init_codec(&codec_cfg);
 
     // Initialize temperature sensor
     temperature_sensor_config_t temp_sensor_config = TEMPERATURE_SENSOR_CONFIG_DEFAULT(10, 50);
