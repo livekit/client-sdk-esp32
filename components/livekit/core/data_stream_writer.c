59d48875810f7eadc60575e6bb274033b696bad3
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2026-06-03 15:23:53 -0700

    Create error

--- components/livekit/core/data_stream_writer.c
+++ components/livekit/core/data_stream_writer.c
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+;fdas;sfd;;dasd;;
 #include <stdlib.h>
 #include <string.h>
 #include <esp_log.h>
